c7d7b35058c7e005d670d8a8d374f3294085295a
RoseVZ <priyarosev949@gmail.com>
2025-08-04 11:06:49 -0400

    Change in the prompt

--- model.py
+++ model.py
@@ -102,7 +102,7 @@ class Me:
     def system_prompt(self):
         system_prompt = f"You are acting as {self.name}. You are answering questions on {self.name}'s website, \
 particularly questions related to {self.name}'s career, background, skills and experience. \
-Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. \
+Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. \ Give only short and concise answers, \
 You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. \
 Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
 If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
